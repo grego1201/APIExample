68aa6d1dc2ef9a94b3aceff95a0cfb8e8f36a083
grego1201 <gregoriopatinoesteo@gmail.com>
2021-07-15 23:52:05 +0200

    Add communicated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
 50.times do |_|
   Person.create( name: Faker::Name.name, birthday: Faker::Date.between(from: 100.years.ago, to: 1.day.ago))
 end
+
+100.times do |i|
+  if i > 25
+    last_communicated = Communicated.find(rand(i))
+  end
+
+  creator = Person.find(rand(50) + 1)
+  receptor = Person.find(rand(50) + 1)
+
+  Communicated.create(creator: creator, receptor: receptor, last_communicated: last_communicated,
+                      subject: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+end
