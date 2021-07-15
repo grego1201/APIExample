00a944f4d519027bbd5feebb28c1d501e35eb76c
grego1201 <gregoriopatinoesteo@gmail.com>
2021-07-16 00:04:07 +0200

    Add class_name to communicateds_sent relation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,3 @@
 class Person < ApplicationRecord
-  has_many :communicateds_sent, foreign_key: 'creator_id'
+  has_many :communicateds_sent, foreign_key: 'creator_id', class_name: 'Communicated'
 end
