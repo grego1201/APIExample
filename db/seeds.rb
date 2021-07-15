1341feebe5f0caa83fb9c7792fc9260c6314ea88
grego1201 <gregoriopatinoesteo@gmail.com>
2021-07-15 23:36:46 +0200

    Create Person model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do |_|
+  Person.create( name: Faker::Name.name, birthday: Faker::Date.between(from: 100.years.ago, to: 1.day.ago))
+end
