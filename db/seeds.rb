bed8cefcac1dc0ebb49b85aabee99e65787aeff7
grego1201 <gregoriopatinoesteo@gmail.com>
2021-07-16 00:25:42 +0200

    Improve seeds code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,25 @@
-50.times do |_|
+people_count = 50
+communicateds_count = 100
+attachments_count = 150
+
+people_count.times do
   Person.create( name: Faker::Name.name, birthday: Faker::Date.between(from: 100.years.ago, to: 1.day.ago))
 end
 
-100.times do |i|
+communicateds_count.times do |i|
+  attrs = {}
+  attrs[:creator_id] = Person.ids.sample
+  attrs[:receptor_id] = Person.ids.sample
+  attrs[:subject] = Faker::Lorem.sentence
+  attrs[:body] = Faker::Lorem.paragraph
+
   if i > 25
-    last_communicated = Communicated.find(rand(i))
+    attrs[:last_communicated_id] = Communicated.ids.sample
   end
 
-  creator = Person.find(rand(50) + 1)
-  receptor = Person.find(rand(50) + 1)
-
-  Communicated.create(creator: creator, receptor: receptor, last_communicated: last_communicated,
-                      subject: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+  Communicated.create(attrs)
 end
 
-150.times do |_|
-  communicated = Communicated.find(rand(100) + 1)
-  Attachment.create(communicated: communicated)
+attachments_count.times do
+  Attachment.create(communicated_id: Communicated.ids.sample)
 end
